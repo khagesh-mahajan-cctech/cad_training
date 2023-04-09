98808d0131ca36ff072c341f9110a9766de27cd3
khagesh-mahajan-cctech <khagesh.mahajan@cctech.co.in>
2023-04-09 22:09:25 +0530

    Collinear And Coplaner Functionality Added
    1) Collinearity of 3 points and collinearity of 2 vector functionality added
    2) Coplanarity of 3 vectors and coplanarity of 4 points functionality added.

--- GeometricVector/GeoUtils.cpp
+++ GeometricVector/GeoUtils.cpp
@@ -50,3 +50,35 @@ int GeomVector::orientation2D(const Point2d& a, const Point2d& b, const Point2d&
 
 	return int (RELATIVE_POSITION::BETWEEN);
 }
+
+bool GeomVector::collinear(const vector3f& v1, const vector3f& v2)
+{
+	auto a = v1[X] * v2[Y] - v1[Y] * v2[X];
+	auto b = v1[Y] * v2[Z] - v2[Z] * v1[Y];
+	auto c = v1[X] * v2[Z] - v1[Z] * v2[X];
+
+	return isEqualD(a, ZERO) && isEqualD(b, ZERO) && isEqualD(c, ZERO);
+}
+
+bool GeomVector::collinear(const Point3d& a, const Point3d& b, const Point3d& c)
+{
+	auto AB = b - a;
+	auto AC = c - a;
+	return collinear(AB, AC);
+}
+
+bool GeomVector::coplaner(const vector3f& v1, const vector3f& v2, const vector3f& v3)
+{
+	float value = scalerTripleProduct(v1, v2, v3);
+
+	return isEqualD(value, ZERO);
+}
+
+bool GeomVector::coplaner(const Point3d& a, const Point3d& b, const Point3d& c, const Point3d& d)
+{
+	auto AB = b - a;
+	auto AC = c - a;
+	auto AD = d - a;
+
+	return coplaner(AB, AC, AD);
+}
